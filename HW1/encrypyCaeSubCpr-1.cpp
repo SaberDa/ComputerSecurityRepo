d95739f16a1f9e064ca2af36f2464ae10dfd31ab
SaberDa <630991493@qq.com>
2020-02-07 16:11:24 -0500

    add decrypt function

--- HW1/encrypyCaeSubCpr-1.cpp
+++ HW1/encrypyCaeSubCpr-1.cpp
@@ -4,9 +4,21 @@
 
 using namespace std;
 
+// Encryp
+// f(a) = (a + 3) mod 26
 string Alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 string psw(string s, int key) {
+    for (int i = 0; i < s.length(); i++) {
+        s[i] = ((s[i] - 'a') + key) % 26 + 'a';
+    }
+    return s;
+}
+
+// Decrypt
+// f(a) = (a + 23) mod 26
+string unpsw(string s, int key) {
+    key = 26 - key;
     for (int i = 0; i < s.length(); i++) {
         s[i] = ((s[i] - 'a') + key) % 26 + 'a';
     }
